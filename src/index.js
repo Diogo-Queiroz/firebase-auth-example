74b49d38b89368154d2d3a684b754de6bb3830d3
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2018-06-14 17:17:43 -0300

    add files, and change index.js
    Add PasswordChange.js, SignOut.js and SignUp.js. Changed the index to exclude the Apps function.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./styles.css"
 import App from './components/App'
-function Apps() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  )
-}
 
 const rootElement = document.getElementById("root")
 ReactDOM.render(<App />, rootElement)
